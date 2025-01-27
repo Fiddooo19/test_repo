2051115aac7c31f1756295600e8cb661cc545230
Fiddooo19 <firdausazizi191199@gmail.com>
2025-01-28 04:09:40 +0800

    create event delete event update event event service xml

---
+++ app/src/main/java/com/example/socialgoodvolunteerapp/EventDetailsActivityAdmin.java
@@ -0,0 +1,146 @@
+package com.example.socialgoodvolunteerapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.socialgoodvolunteerapp.model.Event;
+import com.example.socialgoodvolunteerapp.model.User;
+import com.example.socialgoodvolunteerapp.remote.ApiUtils;
+import com.example.socialgoodvolunteerapp.remote.EventService;
+import com.example.socialgoodvolunteerapp.sharedpref.SharedPrefManager;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class EventDetailsActivityAdmin extends AppCompatActivity {
+
+    private EventService eventService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_event_details_admin);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Initialize UI components
+        ImageView backButton = findViewById(R.id.btnBack);
+
+        Button joinButton = findViewById(R.id.join_button);
+
+        // Back button click listener
+        backButton.setOnClickListener(view -> {
+            finish();
+        });
+
+        // Join button click listener (example placeholder)
+        joinButton.setOnClickListener(view -> {
+            // Code to join the event (to be implemented)
+        });
+
+        // retrieve event details based on selected id
+
+        // get event id sent by EventListActivity, -1 if not found
+        Intent intent = getIntent();
+        int eventId = intent.getIntExtra("event_id", -1);
+
+        // get user info from SharedPreferences
+        SharedPrefManager spm = new SharedPrefManager(getApplicationContext());
+        User user = spm.getUser();
+        String token = user.getToken();
+
+        // get event service instance
+        eventService = ApiUtils.getEventService();
+
+        // execute the API query. send the token and event id
+        eventService.getEvent(token, eventId).enqueue(new Callback<Event>() {
+            @Override
+            public void onResponse(Call<Event> call, Response<Event> response) {
+                // for debug purpose
+                Log.d("MyApp:", "Response: " + response.raw().toString());
+
+                if (response.code() == 200) {
+                    // server return success
+
+                    // get event object from response
+                    Event event = response.body();
+
+                    // get references to the view elements
+                    TextView eventName = findViewById(R.id.tvEventName);
+                    TextView tvDate = findViewById(R.id.tvDate);
+                    TextView tvCategory = findViewById(R.id.tvCategory);
+                    TextView tvLocation = findViewById(R.id.tvLocation);
+                    TextView tvDescription = findViewById(R.id.tvDescription);
+
+                    // set values
+                    eventName.setText(event.getEvent_name());
+                    tvDate.setText(event.getdate());
+                    tvCategory.setText(event.getcategory());
+                    tvLocation.setText(event.getlocation());
+                    tvDescription.setText(event.getdescription());
+
+                }
+                else if (response.code() == 401) {
+                    // unauthorized error. invalid token, ask user to relogin
+                    Toast.makeText(getApplicationContext(), "Invalid session. Please login again", Toast.LENGTH_LONG).show();
+                    clearSessionAndRedirect();
+                }
+                else {
+                    // server return other error
+                    Toast.makeText(getApplicationContext(), "Error: " + response.message(), Toast.LENGTH_LONG).show();
+                    Log.e("MyApp: ", response.toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Event> call, Throwable t) {
+                Toast.makeText(null, "Error connecting", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Get the menu inflater
+        MenuInflater inflater = getMenuInflater();
+        // Inflate the menu using our XML menu file id, options_menu
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    public void clearSessionAndRedirect() {
+        // clear the shared preferences
+        SharedPrefManager spm = new SharedPrefManager(getApplicationContext());
+        spm.logout();
+
+        // terminate this activity
+        finish();
+
+        // forward to Login Page
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/socialgoodvolunteerapp/UpdateEventActivity.java
@@ -0,0 +1,310 @@
+package com.example.socialgoodvolunteerapp;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.DialogFragment;
+
+import com.example.socialgoodvolunteerapp.model.Event;
+import com.example.socialgoodvolunteerapp.model.User;
+import com.example.socialgoodvolunteerapp.remote.ApiUtils;
+import com.example.socialgoodvolunteerapp.remote.EventService;
+import com.example.socialgoodvolunteerapp.sharedpref.SharedPrefManager;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class UpdateEventActivity extends AppCompatActivity {
+
+    // form fields
+    private EditText tvEventName;
+    private EditText tvDescription;
+    private EditText tvLocation;
+    private EditText tvCategory;
+
+    private static TextView tvDate; // static because need to be accessed by DatePickerFragment
+    private static Date date; // static because need to be accessed by DatePickerFragment
+
+    private Event event;  // current book to be updated
+
+    /**
+     * Date picker fragment class
+     * Reference: https://developer.android.com/guide/topics/ui/controls/pickers
+     */
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final Calendar c = Calendar.getInstance();
+            c.setTime(date); // Use the current book created date as the default date in the picker
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+
+            // create a date object from selected year, month and day
+            date = new GregorianCalendar(year, month, day).getTime();
+
+            // display in the label beside the button with specific date format
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            tvDate.setText( sdf.format(date) );
+        }
+    }
+
+    /**
+     * Called when pick date button is clicked. Display a date picker dialog
+     * @param v
+     */
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_update_event);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // retrieve book id from intent
+        // get book id sent by BookListActivity, -1 if not found
+        Intent intent = getIntent();
+        int event_id = intent.getIntExtra("event_id", -1);
+
+        // initialize createdAt to today's date
+        date = new Date();
+
+        // get references to the form fields in layout
+        tvEventName = findViewById(R.id.tvEventName);
+        tvDescription = findViewById(R.id.tvDescription);
+        tvLocation = findViewById(R.id.tvLocation);
+        tvCategory = findViewById(R.id.tvCategory);
+        tvDate = findViewById(R.id.tvDate);
+
+        // retrieve book info from database using the book id
+        // get user info from SharedPreferences
+        SharedPrefManager spm = new SharedPrefManager(getApplicationContext());
+        User user = spm.getUser();
+
+        // get book service instance
+        EventService eventService = ApiUtils.getEventService();
+
+        // execute the API query. send the token and book id
+        eventService.getEvent(user.getToken(), event_id).enqueue(new Callback<Event>() {
+            @Override
+            public void onResponse(Call<Event> call, Response<Event> response) {
+                // for debug purpose
+                Log.d("MyApp:", "Update Form Populate Response: " + response.raw().toString());
+
+                if (response.code() == 200) {
+                    // server return success
+                    // get book object from response
+                    event = response.body();
+
+                    // set values into forms
+                    tvEventName.setText(event.getEvent_name());
+                    tvDescription.setText(event.getdescription());
+                    tvLocation.setText(event.getlocation());
+                    tvCategory.setText(event.getcategory());
+                    tvDate.setText(event.getdate());
+
+                    // parse created_at date to date object
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    try {
+                        // parse created date string to date object
+                        date = sdf.parse(event.getdate());
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else if (response.code() == 401) {
+                    // unauthorized error. invalid token, ask user to relogin
+                    Toast.makeText(getApplicationContext(), "Invalid session. Please login again", Toast.LENGTH_LONG).show();
+                    clearSessionAndRedirect();
+                }
+                else {
+                    // server return other error
+                    Toast.makeText(getApplicationContext(), "Error: " + response.message(), Toast.LENGTH_LONG).show();
+                    Log.e("MyApp: ", response.toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Event> call, Throwable t) {
+                Toast.makeText(null, "Error connecting", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void clearSessionAndRedirect() {
+        // clear the shared preferences
+        SharedPrefManager spm = new SharedPrefManager(getApplicationContext());
+        spm.logout();
+
+        // terminate this MainActivity
+        finish();
+
+        // forward to Login Page
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+
+    }
+
+    /**
+     * Update book info in database when the user click Update Book button
+     * @param view
+     */
+    public void updateEvent(View view) {
+        // get values in form
+        String event_name = tvEventName.getText().toString();
+        String description = tvDescription.getText().toString();
+        String location = tvLocation.getText().toString();
+        String category = tvCategory.getText().toString();
+        String eventDate = tvDate.getText().toString();
+
+        // convert createdAt date to format required by Database - yyyy-MM-dd HH:mm:ss
+        //SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        //String created_at = sdf.format(date);
+
+        // set updated_at to current date and time
+        //String updated_at = sdf.format(new Date());
+
+        Log.d("MyApp:", "Old Event info: " + event.toString());
+
+        // get user info from SharedPreferences
+        SharedPrefManager spm = new SharedPrefManager(getApplicationContext());
+        User user = spm.getUser();
+
+        // update the book object retrieved in when populating the form with the new data.
+        // update all fields excluding the id
+        event.setEvent_name(event_name);
+        event.setdescription(description);
+        event.setlocation(location);
+        event.setcategory(category);
+        event.setdate(eventDate);
+        event.setOrganizer_id(user.getId()); // Set the organizer_id from the logged-in user
+
+        //Log.d("MyApp:", "New Event info: " + event.toString());
+
+        // send request to update the book record to the REST API
+        EventService eventService = ApiUtils.getEventService();
+        Call<Event> call = eventService.updateEvent(user.getToken(),event.getEvent_id(), event.getEvent_name(), event.getdescription(),
+                event.getlocation(), event.getcategory(), event.getdate(), event.getOrganizer_id());
+
+        // execute
+        call.enqueue(new Callback<Event>() {
+            @Override
+            public void onResponse(Call<Event> call, Response<Event> response) {
+
+                // for debug purpose
+                Log.d("MyApp:", "Update Request Response: " + response.raw().toString());
+
+                if (response.code() == 200) {
+                    // server return success code for update request
+                    // get updated book object from response
+                    Event updatedEvent = response.body();
+
+                    // display message
+                    displayUpdateSuccess(updatedEvent.getEvent_name() + " updated successfully.");
+
+
+                }
+                else if (response.code() == 401) {
+                    // unauthorized error. invalid token, ask user to relogin
+                    Toast.makeText(getApplicationContext(), "Invalid session. Please login again", Toast.LENGTH_LONG).show();
+                    clearSessionAndRedirect();
+                }
+                else {
+                    // server return other error
+                    Toast.makeText(getApplicationContext(), "Error: " + response.message(), Toast.LENGTH_LONG).show();
+                    Log.e("MyApp: ", response.toString());
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<Event> call, Throwable t) {
+                displayAlert("Error [" + t.getMessage() + "]");
+                // for debug purpose
+                Log.d("MyApp:", "Error: " + t.getCause().getMessage());
+            }
+        });
+    }
+
+    /**
+     * Displaying an alert dialog with a single button
+     * @param message - message to be displayed
+     */
+    public void displayUpdateSuccess(String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message)
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        // end this activity and forward user to BookListActivity
+                        Intent intent = new Intent(getApplicationContext(), EventListActivityAdmin.class);
+                        startActivity(intent);
+                        finish();
+
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    /**
+     * Displaying an alert dialog with a single button
+     * @param message - message to be displayed
+     */
+    public void displayAlert(String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(message)
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        //do things
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+}
+
